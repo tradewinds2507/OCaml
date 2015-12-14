81caf0fc2f35f25cd9b36646c8c6ec01b5d9eafd
Yash Gupta <yash.gupta@mail.mcgill.ca>
2015-12-14 01:56:37 -0500

    In OCaml-99.ml, Questions 30-32, 38-40, 42 work. 42 has to be rewritten.

--- OCaml-99.ml
+++ OCaml-99.ml
@@ -125,15 +125,138 @@ let isPrime n =
       then acc
       else helper (n-1) (n::acc) )
 in
-  List.fold_left (fun y -> (fun x -> y && not ((n mod x) = 0)) ) true (helper (n-1) [])
+  if (n < 2)
+    then false
+    else List.fold_left (fun y -> (fun x -> y && not ((n mod x) = 0)) ) true (helper (n-1) [])
 
 
 
 
 (* Question 30 *)
 
+let rec gcd x y = match x, y with
+  | 0, _ -> y
+  | _, 0 -> x
+  | _, _ -> gcd y (x mod y)
 
 
+
+
+(* Question 31 *)
+
+let coprime x y = if (gcd x y = 1)
+    then true
+    else false
+
+
+
+
+(* Question 32 *)
+
+let phi m =
+  let rec helper n acc = ( if (n < 1)
+      then acc
+      else helper (n-1) (n::acc) )
+in
+  lengthOfList (List.filter (fun x -> coprime x m) (helper (m-1) []))
+
+
+
+
+(* Question 33 *)
+
+
+
+
+(* Question 38 *)
+
+let goldbach n = if (n mod 2 = 0)
+    then let rec helper n acc =
+        if (isPrime acc) && (isPrime (n-acc))
+          then (acc, n-acc)
+          else helper n (acc+1)
+      in
+        helper n 1
+    else (0, n)
+
+
+
+
+(* Question 39 *)
+
+let goldbach_list x y = 
+  let rec helper x y acc = if (x < y)
+      then ( match (goldbach x) with
+          | (0, _) -> helper (x+1) y acc
+          | z -> helper (x+1) y (acc@[(x, z)]) )
+      else acc
+in
+  helper x y []
+
+let goldbach_limit x y z =
+  let l = goldbach_list x y
+  in
+    let rec helper l' acc = ( match l' with
+      | [] -> acc
+      | (p, (q, r))::ls -> if (q > z) && (r > z)
+          then helper ls acc@[(p, (q, r))]
+          else helper ls acc )
+in
+  helper l []
+
+
+
+
+(* Logic and Codes *)
+
+type bool_exp =
+  | Var of string
+  | Not of bool_exp
+  | And of bool_exp * bool_exp
+  | Or of bool_exp * bool_exp
+
+(* Question 40 *)
+
+let table2 x y e =
+  let rec eval (x, y) (p, q) e = ( match e with
+    | Var z -> if (z = x) then p else q
+    | Not e' -> not (eval (x, y) (p, q) e')
+    | And (e1, e2) -> (eval (x, y) (p, q) e1) && (eval (x, y) (p, q) e2)
+    | Or (e1, e2) -> (eval (x, y) (p, q) e1) || (eval (x, y) (p, q) e2)  )
+in
+  [ (true, true, (eval (x, y) (true, true) e));
+    (true, false, (eval (x, y) (true, false) e));
+    (false, true, (eval (x, y) (false, true) e));
+    (false, false, (eval (x, y) (false, false) e)) ]
+
+
+
+
+(* Question 41 *)
+
+
+
+
+(* Question 42 *)
+
+let gray n =
+  let rec helper1 k acc = ( match k with
+    | 0 -> acc
+    | _ -> helper1 (k/2) ((string_of_int (k mod 2))^acc) )
+  in
+    let rec helper2 n acc = ( if (n < 0)
+        then acc
+        else helper2 (n-1) ((helper1 n "")::acc) )
+in
+  helper2 (Pervasives.truncate (2.0 ** (Pervasives.float n) -. 1.0 )) []
+
+
+
+
+(* Question 43 *)
+(*
+let huffman fs = 
+*)
 
 
 
